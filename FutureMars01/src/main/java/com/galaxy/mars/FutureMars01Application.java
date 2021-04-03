4867bcc0531e6baa84d1123e342a92829e5ff635
eunoia <eunoia@localhost>
2021-04-03 15:59:23 +0900

    first push

---
+++ FutureMars01/src/main/java/com/galaxy/mars/FutureMars01Application.java
@@ -0,0 +1,13 @@
+package com.galaxy.mars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutureMars01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FutureMars01Application.class, args);
+	}
+
+}
