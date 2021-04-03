0ba1b2f4c7140ca06987b7e78b1faac635482cff
joseongjin <joseongjin@172.30.1.38>
2021-04-03 17:14:57 +0900

    Buildingcon

---
+++ FutureMars01/src/main/java/com/galaxy/mars/building/control/BuildingController.java
@@ -0,0 +1,29 @@
+package com.galaxy.mars.building.control;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.galaxy.mars.building.service.BuildingService;
+
+@Component
+public class BuildingController {
+
+	@Autowired
+	BuildingService service;
+	
+	@RequestMapping(value = "index.do")
+	public String setStairInfo() {
+		service.setStair1();
+		
+		return "index.jsp";
+	}
+	
+	@RequestMapping(value = "page.do")
+	public String page() {
+		service.setStair1();
+		
+		return "page.jsp";
+	}
+
+}
