51b3a4b14293d82b952cc1f9e5909b3b9209c7e4
eunoia <eunoia@localhost>
2021-04-03 16:36:09 +0900

    101 yeah~!

--- FutureMars01/src/main/java/com/galaxy/mars/building/service/BuildingService.java
+++ FutureMars01/src/main/java/com/galaxy/mars/building/service/BuildingService.java
@@ -5,8 +5,10 @@ public interface BuildingService {
 	public void setStair1();
 	public void setStair2();
 	public void setStair3();
+	public void setStair4();
 	
 	
 	public void setStair100();
+	public void setStair101();
 	
 }
