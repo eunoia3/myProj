f95cf0f45485ea4d5eaa797153677e458d3ce53a
joseongjin <joseongjin@172.30.1.38>
2021-04-03 16:51:42 +0900

    a

--- FutureMars01/src/main/java/com/galaxy/mars/building/service/BuildingService.java
+++ FutureMars01/src/main/java/com/galaxy/mars/building/service/BuildingService.java
@@ -7,11 +7,12 @@ public interface BuildingService {
 	public void setStair3();
 	public void setStair4();
 	public void setStair5();
+	public void setStair6();
 	
 	
 	public void setStair100();
 	
 	public void setStair102();
 	public void setStair101();
-	
+	public void setStair1000();
 }
