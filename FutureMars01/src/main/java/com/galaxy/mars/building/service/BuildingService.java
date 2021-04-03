1bd10d54c9121dcf2999e566a26104d338d51871
joseongjin <joseongjin@172.30.1.38>
2021-04-03 16:22:58 +0900

    First Commit

---
+++ FutureMars01/src/main/java/com/galaxy/mars/building/service/BuildingService.java
@@ -0,0 +1,8 @@
+package com.galaxy.mars.building.service;
+
+public interface BuildingService {
+
+	public void setStair1();
+	public void setStair2();
+	
+}
---
+++ FutureMars01/src/main/java/com/galaxy/mars/building/vo/BuildingVO.java
@@ -0,0 +1,8 @@
+package com.galaxy.mars.building.vo;
+
+
+public class BuildingVO {
+	private String Stair1 = "";
+	private String Stair2 = "";
+	
+}
